3c09bf3717a25dc94b93756825ac24489d14ea41
AINON  M. RHETICE <malyckanon@gmail.com>
2022-12-23 09:07:22 +0100

    deploying change 1

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn instagramD.wsgi --log-file -
--- instagramD/settings.py
+++ instagramD/settings.py
@@ -10,12 +10,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rxuh4vdt+c0x#^fzdhj3k_1ostt5k_va*tmu+579nzwtd!$!y='
+SECRET_KEY = 'django-insecure-rxuh4vdt+c0x#^fzdhj3k_1ostt5k_va*tmu+579nzwtd!$!y=@xjl7me'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
